efc08cc15d19f59f9e6be587a10cf4f7de5f561a
Gary Choi <78285905+garychd214@users.noreply.github.com>
2022-01-11 17:54:38 -0500

    Script Update

--- Airmon.py
+++ Airmon.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
 __author__ = "Gary Choi"
 __project__= "Graduate Research Project"
@@ -6,7 +6,45 @@ __name__="WAAT"
 
 import os
 import sys
-from subprocess import run, PIPE
+import subprocess
+import asyncio
+import pyrcrack
 
-res = run('apt-cache policy aircrack-ng', shell=True, stdout=PIPE, stderr=PIPE, check=True)
-print(res.returncode, res.stdout, res.stderr)
+from rich.console import Console
+from rich.prompt import Prompt
+
+res = subprocess.run('dpkg-query -l aircrack-ng', shell=True)
+print(res.returncode)
+
+if(res.returncode != 0):
+    print("Please install aircrack-ng")
+    exit(1)
+
+def banner():
+    print ("\n+----------------------------------------------------------------+")
+    print ("|WAAT (Wireless Analysis and Assessment Tool)                    |")
+    print ("|Coded by Gary Choi                                              |")
+    print ("|Repo: https://github.com/garychd214/WAAT                        |")
+    print ("+----------------------------------------------------------------+\n")
+
+
+async def scan_for_targets():
+    """Scan for targets, return json."""
+    console = Console()
+    console.clear()
+    console.show_cursor(False)
+    airmon = pyrcrack.AirmonNg()
+
+    interface = Prompt.ask(
+        'Select an interface',
+        choices=[a['interface'] for a in await airmon.interfaces])
+
+    async with airmon(interface) as mon:
+        async with pyrcrack.AirodumpNg() as pdump:
+            async for result in pdump(mon.monitor_interface):
+                console.clear()
+                console.print(result.table)
+                await asyncio.sleep(2)
+
+
+asyncio.run(scan_for_targets())
