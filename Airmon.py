20ff6d9e46d2bbb8359546f7cc7138058df4a1a2
Gary Choi <78285905+garychd214@users.noreply.github.com>
2022-01-11 19:27:30 -0500

    WAAT updates

--- Airmon.py
+++ Airmon.py
@@ -4,47 +4,54 @@ __author__ = "Gary Choi"
 __project__= "Graduate Research Project"
 __name__="WAAT"
 
-import os
-import sys
 import subprocess
-import asyncio
-import pyrcrack
+import re
+import csv
+import os
+import time
+import shutil
+from datetime import datetime
 
-from rich.console import Console
-from rich.prompt import Prompt
-
-res = subprocess.run('dpkg-query -l aircrack-ng', shell=True)
-print(res.returncode)
-
-if(res.returncode != 0):
-    print("Please install aircrack-ng")
-    exit(1)
+wireless_interface = []
 
 def banner():
-    print ("\n+----------------------------------------------------------------+")
-    print ("|WAAT (Wireless Analysis and Assessment Tool)                    |")
-    print ("|Coded by Gary Choi                                              |")
-    print ("|Repo: https://github.com/garychd214/WAAT                        |")
-    print ("+----------------------------------------------------------------+\n")
+    print ("\n+------------------------------------------+")
+    print ("|     _     _  _______  _______  _______     |")
+    print ("|    | | _ | ||   _   ||   _   ||       |    |")
+    print ("|    | || || ||  |_|  ||  |_|  ||_     _|    |")
+    print ("|    |       ||       ||       |  |   |      |")
+    print ("|    |       ||       ||       |  |   |      |")
+    print ("|    |   _   ||   _   ||   _   |  |   |      |")
+    print ("|    |__| |__||__| |__||__| |__|  |___|      |")
+    print ("|                                            |")
+    print ("| Coded by Gary Choi                         |")
+    print ("| Repo: https://github.com/garychd214/WAAT   |")
+    print ("+--------------------------------------------+\n")
 
+def clear():
+    _ = subprocess.call('clear')
 
-async def scan_for_targets():
-    """Scan for targets, return json."""
-    console = Console()
-    console.clear()
-    console.show_cursor(False)
-    airmon = pyrcrack.AirmonNg()
+clear()
+banner()
 
-    interface = Prompt.ask(
-        'Select an interface',
-        choices=[a['interface'] for a in await airmon.interfaces])
+# If script doesn't run with super user privileges, break the script
+if not 'SUDO_UID' in os.environ.keys():
+    print("Please run the script with sudo")
+    exit()
 
-    async with airmon(interface) as mon:
-        async with pyrcrack.AirodumpNg() as pdump:
-            async for result in pdump(mon.monitor_interface):
-                console.clear()
-                console.print(result.table)
-                await asyncio.sleep(2)
+# Check if aircrack-ng package is installed
+res = subprocess.run('dpkg-query -l aircrack-ng', shell=True)
+if(res.returncode != 0):
+    print("Please install aircrack-ng")
+    exit()
+else:
+    clear()
+    banner()
 
+# wlan interface finder
+wlan_finder = re.compile("^wlan[0-9]+")
+# Find all wireless interface from iwconfig command
+wlan_int_list = wlan_finder.findall(subprocess.run(["iwconfig"], capture_output=True).stdout.decode())
 
-asyncio.run(scan_for_targets())
+# Debug Check Wlan list
+print(wlan_int_list)
